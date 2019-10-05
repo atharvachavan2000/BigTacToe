b4e3d12ea73a53096120f5ea052a236517f791d7
Rohan Bhide <rohanbhide19@hotmail.com>
2019-10-05 22:27:22 +0530

    pvp almost done

--- BigTacToe.cpp
+++ BigTacToe.cpp
@@ -7,11 +7,13 @@ using namespace std;
 
 class Game{
     vector<vector<int>> board;
+    int activePlayer;
 
 public:
     Game(); //init board & add option to choose mode later
     vector<int> validateMove(string move); //check if move is valid
     void applyMove(vector<int> move); //apply move if valid
+    int winner(); //who won the game
     void displayBoard(); //display 9x9 board with proper A-I & 1-9 labels
     void playHuman(); //human vs human mode
     void playComp(); //human vs cpu mode
@@ -20,22 +22,89 @@ public:
 };
 
 Game::Game(){
-    board.resize(9, vector<int>(9, 0)); //init to 0
+    activePlayer = 0; //assume player 1 starts
+    board.resize(9, vector<int>(9,-1)); //init to -1
 }
 
 void Game::displayBoard(){
-    cout<<"\t";
-    for(int i = 0; i <board.size(); i++){
-        for(int j = 0; j <board[i].size(); j++){
-            if(j == 0 && i != 0)
-                cout<<char(i + 64)<<"\t";
+    for(int i = 0; i <=board.size(); i++){
+        for(int j = 0; j <=board.size(); j++){
+            if(j == 0)
+                if(i != 0)
+                    cout<<char(i + 64)<<"\t";
+                else
+                    cout<<"\t";
             else if(i == 0)
-                cout<<j+1<<"\t";
+                cout<<j<<"\t";
             else{
-                cout<<board[i][j]<<"\t";
+                if(board[i-1][j-1] == 0)
+                    cout<<"X"<<"\t";
+                else if(board[i-1][j-1] == 1)
+                    cout<<"O"<<"\t";
+                else
+                    cout<<"_"<<"\t";
             }
         }
-        cout<<endl;
+        cout<<endl<<endl;
+    }
+}
+
+vector<int> Game::validateMove(string move){
+    //invalid input
+    if(move.size() != 2)
+        return {};
+    else if(move[0] < 'A' || move[0] > 'I')
+        return {};
+    else if(move[1] < '1' || move[1] > '9')
+        return {};
+
+    //convert to int coordinates
+    int rowPos = move[0] - 65;
+    int colPos = move[1] - 49;
+
+    //validate
+    if(board[rowPos][colPos] != -1)
+        return {};
+    else
+        return {rowPos, colPos};
+}
+
+void Game::applyMove(vector<int> move){
+    board[move[0]][move[1]] = activePlayer;
+    activePlayer = !activePlayer;
+}
+
+int Game::winner(){
+    //check board for pairs of 3
+    for(int i = 0; i  < board.size(); i++){
+        for(int j = 0; j < board.size(); j++){
+            if(board[i][j] != -1){
+                //vertical
+                
+            }
+        }
+    }
+    return -1;
+}
+
+void Game::playHuman(){
+    string move;
+    displayBoard();
+    while(1){
+        cout<<"Player "<<activePlayer + 1<<"'s Turn : "<<endl;
+        cin>>move;
+        vector<int> m = validateMove(move);
+        if(!m.empty())
+            applyMove(m);
+        else
+            cout<<"Invalid Move!!!"<<endl;
+
+        if(winner() != -1){
+            cout<<"Player "<<activePlayer + 1<<" Won!!!"<<endl;
+            break;
+        }
+
+        displayBoard();
     }
 }
 
@@ -45,6 +114,8 @@ class Bot{
 
 int main(){
     Game g;
+    vector<int> v = g.validateMove("A1");
+    g.applyMove(v);
     g.displayBoard();
     return 0;
 }
